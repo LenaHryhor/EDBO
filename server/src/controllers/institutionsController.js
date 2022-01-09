b5b822a411bfca5962488e2a0edd8990b3516624
Olena Grigor <hrigor21@gmail.com>
2022-01-09 18:35:07 +0200

    fix address and add "update institutions"

--- server/src/controllers/institutionsController.js
+++ server/src/controllers/institutionsController.js
@@ -1,4 +1,4 @@
-const { getInstitutionsByRegion, getInstitutionsByRegionAndName, createOneInstitution } = require('../services/institutionsService')
+const { getInstitutionsByRegion, getInstitutionsByRegionAndName, createOneInstitution, updateOneInstitution } = require('../services/institutionsService')
 
 const getInstitutions = async(req, res) => {
     const { region, global_type, name } = req.query;
@@ -16,13 +16,16 @@ const getInstitutions = async(req, res) => {
 }
 
 const createInstitution = async(req, res) => {
-    const { code_edbo, long_name, short_name, code_identification, type, form, adress, region, city, phone, email, site, year_foundation, unit_institution, postal_code, global_type, position, name, surname, patronymic, p_series, p_number, authority_code, issue_date, birthday_date } = req.body;
-    await createOneInstitution({ code_edbo, long_name, short_name, code_identification, type, form, adress, region, city, phone, email, site, year_foundation, unit_institution, postal_code, global_type, position, name, surname, patronymic, p_series, p_number, authority_code, issue_date, birthday_date })
+    const { code_edbo, long_name, short_name, code_identification, type, form, adress, region, phone, email, site, year_foundation, unit_institution, postal_code, global_type, position, name, surname, patronymic, p_series, p_number, authority_code, issue_date } = req.body;
+    await createOneInstitution({ code_edbo, long_name, short_name, code_identification, type, form, adress, region, phone, email, site, year_foundation, unit_institution, postal_code, global_type, position, name, surname, patronymic, p_series, p_number, authority_code, issue_date})
     res.status(200).json({ message: "Заклад created successfully" })
 }
 
 const updateInstitution = async(req, res) => {
-    res.json();
+    const {institution_id, code_edbo, long_name, short_name, code_identification, type, form, adress, region, phone, email, site, year_foundation, unit_institution, postal_code, global_type, position, name, surname, patronymic, p_series, p_number, authority_code, issue_date} = req.query;
+    console.log(`${code_edbo} ${long_name} ${p_series} ${p_number} ${name} ${surname}`);
+    await updateOneInstitution({institution_id, code_edbo, long_name, short_name, code_identification, type, form, adress, region, phone, email, site, year_foundation, unit_institution, postal_code, global_type, position, name, surname, patronymic, p_series, p_number, authority_code, issue_date})
+    res.status(200).json({ message: "Заклад  успішно оновлено" })
 }
 
 
